c0473ae85090780166751b5f15a1e0e846d5ea8b
SauvageRene <renegarcia1864@gmail.com>
2020-12-04 13:13:47 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 class Application < Sinatra::Base
   # Write your code here!
+  get '/' do
+    erb :index
+  end
 
+  # New route to respond to theform submission
+  post '/greet' do
+    erb :greet
+  end
+  
 end
